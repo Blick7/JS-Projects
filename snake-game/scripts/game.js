98ecb7ec2b574ffd432417714c7ab03ad15c5f93
Blick <45530302+Blick7@users.noreply.github.com>
2022-02-22 13:29:51 +0200

    feat: add game loop

---
+++ snake-game/scripts/game.js
@@ -0,0 +1,33 @@
+import { snake, drawSnake } from "./snake.js";
+
+const gameBoard = document.querySelector(".game-board");
+let startTimeStamp = null;
+
+// create background dots
+const createDots = () => {
+  for (let i = 0; i < 400; i++) {
+    let bgDot = document.createElement("div");
+    bgDot.classList.add("circle");
+    gameBoard.append(bgDot);
+  }
+};
+createDots();
+
+// start game loop
+const gameLoop = (timeStamp) => {
+  window.requestAnimationFrame(gameLoop);
+  let timeSpent = (timeStamp - startTimeStamp) / 1000; // get seconds
+  /* Logging the time spent in the game loop. */
+  //   console.log(timeSpent);
+  if (timeSpent < 1 / snake.speed) return;
+
+  startTimeStamp = timeStamp;
+  gameUpdate();
+};
+window.requestAnimationFrame(gameLoop);
+
+const gameUpdate = () => {
+  //
+  console.log("update");
+  drawSnake(gameBoard);
+};
---
+++ snake-game/scripts/snake.js
@@ -0,0 +1,10 @@
+export let snake = {
+  body: [10],
+  speed: 1,
+};
+
+export const drawSnake = (gameBoard) => {
+  // create snake body square element
+  let snakeBody = document.createElement("div");
+  snakeBody.classList.add("snake-piece");
+};
